0d4eb403fdc843f240a54cf7db11865b68a47925
JMG <jmaximeg64@gmail.com>
2019-03-28 12:38:58 +0100

    my-travel quest content added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Travel from './travel'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Travel 
+        destination='Neptune'
+         country='Milky Way'
+         photo='https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/1200px-Neptune_Full.jpg' 
+         distance='2.9 Billions Miles'/>
+
+         <Travel 
+         destination='Jupiter'
+         country='Milky Way'
+         photo='http://cdn.sci-news.com/images/enlarge3/image_4461e-Jupiter.jpg'
+         distance='365 Millions Miles'
+         />
       </div>
     );
   }
---
+++ src/travel.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { timingSafeEqual } from 'crypto';
+
+
+class Travel extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.props.destination}</h1>
+                <h3>{this.props.country}</h3>
+                <p>This planet is at {this.props.distance} from Earth</p>
+                <img src={this.props.photo} />
+             </div>
+        )
+
+    }
+}
+
+
+export default Travel;
